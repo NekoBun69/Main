cb61ea18b7979a0b399f4928ff6bb7ba429c7cb5
Felipe Freire dos Santos <surfy.fyber@gmail.com>
2026-09-23 19:05:13 -0300

    att 3.0 modal buton

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
 const botaoMenu = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
 
+
+const formulario = document.querySelector("form");
+const modal = document.querySelector("#modal-sucesso");
+const botaoFecharModal = document.querySelector("#fechar-modal");
+
+
 if (botaoMenu && menu) {
     botaoMenu.addEventListener("click", function () {
         menu.classList.toggle("ativo");
+    });
+
+}
+
+if (formulario && modal && botaoFecharModal) {
+
+    formulario.addEventListener("submit", function (evento) {
+
+        evento.preventDefault();
+
+        modal.classList.add("ativo");
+        modal.setAttribute("aria-hidden", "false");
+
+    });
+
+    botaoFecharModal.addEventListener("click", function () {
+
+        modal.classList.remove("ativo");
+        modal.setAttribute("aria-hidden", "true");
+
+        formulario.reset();
+
     });
 
 }
